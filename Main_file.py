b8759cf3bd220cd7883931e7df60f7b712d93c15
Mak7-kurochka <129383186+Mak7-kurochka@users.noreply.github.com>
2026-01-15 13:20:10 +0100

    Refactor Graph and Ant classes with improvements
    Updated various functions and fixed typos in the code. Added pheromone matrix initialization and adjusted parameters in several methods.

--- Main_file.py
+++ Main_file.py
@@ -6,6 +6,8 @@ import time
 import math
 from scipy import stats
 
+plt.style.use('seaborn-v0_8-pastel')
+
 class Node:
     def __init__(self,value):
         self.value = value
@@ -30,6 +32,7 @@ class Ant:
     def __init__(self):
         self.passed = []
         self.dist = 0
+        self.distances = []
         self.routes = []
 
 class Graph:
@@ -37,6 +40,13 @@ class Graph:
         self.nodes = {}
         self.edges = {}
         self.matrix = matrix
+        self.pheromone_matrix = self.pheromones()
+
+    def pheromones(self):
+        n = len(self.matrix)
+        p_matrix = np.ones((n,n))
+        np.fill_diagonal(p_matrix, 0)
+        return p_matrix
 
     def add_or_get(self,name):
         if name in self.nodes.keys():
@@ -66,7 +76,8 @@ class Graph:
 
     def depth_traversal(self,passed=[]):
         values = list(self.nodes.values())
-        stack = []
+        stack = [values[0]]
+        self.depth_traversal_wrap(passed,stack)
         for value in values:
             if value not in passed:
                 stack.append(value)
@@ -82,15 +93,16 @@ class Graph:
                 self.depth_traversal_wrap(passed,stack)
         stack.pop()
     
-    def breadth_traversal(self,passed=[]):
+    def breath_traversal(self,passed=[]):
         values = list(self.nodes.values())
-        queue = []
+        queue = [values[0]]
+        self.breath_traversal_wrap(passed,queue)
         for value in values:
             if value not in passed:
                 queue.append(value)
-                self.breadth_traversal_wrap(passed,queue)
+                self.breath_traversal_wrap(passed,queue)
 
-    def breadth_traversal_wrap(self,passed,queue):
+    def breath_traversal_wrap(self,passed,queue):
         if len(queue) == 0:
             return
         node = queue[0]
@@ -100,7 +112,7 @@ class Graph:
         for edge in node.edges:
             if edge.dir not in passed and edge.dir not in queue:
                 queue.append(edge.dir)
-        self.breadth_traversal_wrap(passed,queue)
+        self.breath_traversal_wrap(passed,queue)
 
     def find_path(self, start, end):
         start_node = self.nodes[start]
@@ -157,6 +169,7 @@ class Graph:
                 end_node.edges.remove(edge)
                 break  
         del self.edges[frozenset([start_node.value,end_node.value])]
+        self.matrix[start][end] = 0
            
     def delete_node(self,value):
         node = self.nodes[value]
@@ -232,10 +245,10 @@ class Graph:
         routes = []
         for route in permutations(names):
             routes.append([start_city] + list(route) + [start_city])
-        routes_weights = []
+        routes_lengths = []
         for route in routes:
-            routes_weights.append(self.calculate_distance(route))
-        return min(routes_weights)
+            routes_lengths.append(self.calculate_distance(route))
+        return min(routes_lengths)
 ###brute force end
 
 ###random swapping start
@@ -243,10 +256,9 @@ class Graph:
         local_route = route.copy()
         del(local_route[0])
         del(local_route[-1])
-        first_city, second_city = 0,0
-        while first_city == second_city:
-            first_city = random.choice(local_route)
-            second_city = random.choice(local_route)
+        first_city = random.choice(local_route)
+        del(local_route[local_route.index(first_city)])
+        second_city = random.choice(local_route)
 
         first_city_index = route.index(first_city)
         second_city_index = route.index(second_city)
@@ -255,7 +267,7 @@ class Graph:
 
         return route
 
-    def random_swaping(self,route_llst=None,batch=1000,s=0.05):
+    def random_swaping(self,route_llst=None,batch=1000,k=0.05):
         if not route_llst:
             route = (list(range(0,len(self.nodes.keys()))))
             random.shuffle(route)
@@ -263,11 +275,11 @@ class Graph:
             route_dist = self.calculate_distance(route)
         else:
             route_dist, route = route_llst
-            
+
         i = 0
         Y = 0
         while i < 10**6:
-            
+
             if Y >= batch:
                 break
             
@@ -279,7 +291,7 @@ class Graph:
                 route = new_route
                 route_dist = new_route_dist
                 Y = 0
-            elif (new_route_dist - route_dist)/route_dist < s:
+            elif (new_route_dist - route_dist)/route_dist <= k:
                 Y += 1
 
             i += 1
@@ -288,7 +300,7 @@ class Graph:
 ###random swapping end
 
 ###2-opt start
-    def two_opt(self,path=None):
+    def two_opt(self, path=None, batch=10, s=1):
         if not path:
             route = (list(range(0,len(self.nodes.keys()))))
             random.shuffle(route)
@@ -297,21 +309,23 @@ class Graph:
             route = path[1]
         iteration = 0
         i = 0
+        Y = 0
         improved = True
-        while i<len(route)-1:
+        n = len(route)
+        while i<n-2:
 
             if iteration >= 10**6:
-                
                 break
 
             if improved:
+
                 i=0
                 improved=False
                 
             A = route[i]
             B = route[i+1]
             j=i+2
-            while j<len(route)-1:
+            while j<n-2:
                 C = route[j]
                 D = route[j+1]
                 org_dist = self.calculate_distance([A,B]) + self.calculate_distance([C,D])
@@ -319,15 +333,18 @@ class Graph:
                 if new_dist < org_dist:
                     route = route[:i]+[A,C]+route[i+2:j][::-1]+[B,D]+route[j+2:]
                     improved = True
+
                     break
+
                 j +=1
+
             i += 1
             iteration += 1
         return self.calculate_distance(route),route
 ###2-opt end
 
 ###Simulated annealing starts
-    def simulated_annealing(self,path=None,t_max=10000,t_min=0.1,a=0.995,i_max=500):##t_max was set to have acceptance rate > 0.9
+    def simulated_annealing(self,path=None,t_max=5000,t_min=0.1,a=0.995,i_max=500):
         if not path:
             current_route = (list(range(0,len(self.nodes.keys()))))
             random.shuffle(current_route)
@@ -365,20 +382,29 @@ class Graph:
 
         return best_distance, best_route
     
-    def choose_the_town(self,ant,a=2,b=5):
+    def choose_the_town(self,ant,a=3, b=2.5):
         current_node = ant.passed[-1]
-        towns = list(set(self.nodes.keys())-set(ant.passed))
-        t_n = []
-        for town in towns:
-            edge = self.edges[frozenset([town,current_node])]
-            t_n.append((edge.pheromone**a) * (edge.visibility**b))
-        total = sum(t_n)
-        probabilities = [t_n[i]/total for i in range(len(t_n))]   
+        visited = set(ant.passed)
+        
+        towns = [node for node in self.nodes.keys() if node not in visited]
+
+        pheromones = self.pheromone_matrix[towns, current_node]
+
+        distances = self.matrix[towns, current_node]
+
+        probability = (pheromones ** a) * (1/distances ** b)
+
+        total = sum(probability)
+        if total == 0:
+            probabilities = [1 / len(towns)] * len(towns)
+        else:
+            probabilities = probability / total
+
         town = np.random.choice(towns, p=probabilities)
+
         return town
 
-
-    def update_pheromones(self,p=0.3,Q=1000):
+    def update_pheromones_1(self,p=0.5,Q=1000):
         for edge in self.edges.values():
             delta_trail = 0
             if edge.ants:
@@ -386,52 +412,74 @@ class Graph:
                     delta_trail += Q/ant.dist
             edge.pheromone = (1-p)*edge.pheromone + delta_trail
             edge.ants = []
+            
+    def update_pheromones(self,p=0.3,Q=1000):
+        self.pheromone_matrix *= (1-p)
 
-    def ant_system(self,m=100,s=0.03,batch=10):
+        for edge in self.edges.values():
+            delta_trail = 0
+            if edge.ants:
+                for ant in edge.ants:
+                    delta_trail += Q/ant.dist
+
+                u = edge.parent.value
+                v = edge.dir.value
+
+                self.pheromone_matrix[u][v] += delta_trail
+                self.pheromone_matrix[v][u] += delta_trail
+                edge.ants = []
+    
+    def ant_system(self,m=100,s=0.01,batch=10):
         ants = [Ant() for _ in range(m)]
         keys = list(self.nodes.keys())
         best_route = []
         best_dist = np.inf
         Y = 0
         i = 0
-        while i < 2500:
-            
+        while i < 50:
+
             if Y >= batch:
+
                 break
-                
+
             for ant in ants:
-                
+                ant.distances.append(ant.dist)
                 ant.routes.append(ant.passed)
                 ant.passed = []
                 ant.dist = 0
                 ant.passed.append(random.choice(keys))
 
-                while len(ant.passed) < len(keys):
+            while len(ants[0].passed) < len(keys):
+                for ant in ants:
                     town = self.choose_the_town(ant)
                     edge = self.edges[frozenset([town,ant.passed[-1]])]
                     ant.passed.append(town)
                     ant.dist += edge.weight
                     edge.ants.append(ant)
             for ant in ants:
-                edge = self.edges[frozenset([ant.passed[-1],ant.passed[0]])]
-                ant.dist += edge.weight
+                last_edge = self.edges[frozenset([ant.passed[-1],ant.passed[0]])]
+                ant.dist += last_edge.weight
                 ant.passed.append(ant.passed[0])
-                edge.ants.append(ant)
+                last_edge.ants.append(ant)
                 if ant.dist < best_dist:
                     best_route = ant.passed
                     best_dist = ant.dist
                     Y = 0
+
                 elif ((ant.dist - best_dist)/best_dist) < s:
                     Y += 1
             self.update_pheromones()
             i += 1
 
         return (best_dist,best_route)
-    
+
 def create_matrix(n):
-    matrix = np.random.randint(400, 5000, size=(n, n))
-    matrix = (matrix + matrix.T) / 2
-    np.fill_diagonal(matrix, 0)
+    x = np.random.randint(0, 689, size=(n))
+
+    y = np.random.randint(0, 649, size=(n))
+
+    matrix = np.sqrt((x[:, np.newaxis] - x)**2 + (y[:, np.newaxis] - y)**2)
+
     return matrix
 
 def sample_size(sample,alpha=0.05):
@@ -441,12 +489,11 @@ def sample_size(sample,alpha=0.05):
     allow_eror = alpha * mean
 
     h = ((u_alpha ** 2) * (std ** 2))/allow_eror ** 2
+    return h.astype(int)
 
-    return h
-
-def confirm_normal_distribution(sample,N,alpha=0.05):
-    obs_frequency, ints, _ = plt.hist(sample)
-
+def confirm_normal_distribution(sample, N, alpha=0.05):
+    k = np.ceil(np.sqrt(N)).astype(int)
+    obs_frequency, ints, _ = plt.hist(sample, bins=k)
     intervals = list(zip(ints, ints[1:]))
 
     mean = np.average(sample)
@@ -462,27 +509,33 @@ def confirm_normal_distribution(sample,N,alpha=0.05):
 
     chi2 = sum([((obs_frequency[i]-exp_frequency[i])**2)/exp_frequency[i] for i in range(len(obs_frequency))])
 
-    dof = len(intervals) - 1 - 2
+    dof = len(intervals) - 2 - 1
 
     critical_value = stats.chi2.ppf(1 - alpha, dof)
 
     print(chi2,critical_value)
 
-    return chi2<critical_value
+    return (chi2,critical_value,chi2<critical_value)
 
 def pilot_experiment(lof,distance_matrix,minimum,step):
     file_name = input('Please enter the name of .txt file for distances: ')
     file_name_time = input('Please enter the name of .txt file for times: ')
+    file_n_name = input('Please enter the name of .txt file for N: ')
     results_file = open(file_name,'a',buffering=1)
     results_time_file = open(file_name_time,'a',buffering=1)
+    result_n_file = open(file_n_name,'a',buffering=1)
     results = {}
     results_time = {}
+    results_n = {}
     for func in lof:
         results_file.write(f'{func}\n')
         results_time_file.write(f'{func}\n')
+        result_n_file.write(f'{func}\n')
+
         print(f'{func}')
         results[func] = []
         results_time[func] = []
+        results_n[func] = []
         for n in range(minimum,len(distance_matrix)+1,step):
             submatrix = distance_matrix[0:n,0:n]
             names = np.arange(0,n)
@@ -496,19 +549,22 @@ def pilot_experiment(lof,distance_matrix,minimum,step):
                 res, _ = getattr(graph_temp,func)()
                 sample_times.append(time.time() - time_before)
                 sample.append(res)
-                print(_)
+
             print(confirm_normal_distribution(sample,N))
-            if sample_size(sample) <= N:
+            n_obl = sample_size(sample)
+            if n_obl <= N:
                 results[func].append(np.mean(sample))
                 results_time[func].append(np.mean(sample_times))
+                results_n[func].append(np.mean(n_obl))
                 results_file.write(f'{np.mean(sample)}\n')
                 results_time_file.write(f'{np.mean(sample_times)}\n')
+                result_n_file.write(f'{n_obl}\n')
             else:
                 while sample_size(sample) > N:
                     N = sample_size(sample)
                     sample_ns = []
                     sample_times_ns = []
-                    print(f"For {func} sample is too small, it has to be {sample_size(sample)}")
+                    print(f"For {func} sample is too small, it has to be at least {sample_size(sample)}")
                     for _ in range(N):
                         graph_temp = Graph(submatrix)
                         graph_temp.add_edge(submatrix,names)
@@ -516,13 +572,17 @@ def pilot_experiment(lof,distance_matrix,minimum,step):
                         res, _ = getattr(graph_temp,func)()
                         sample_times_ns.append(time.time() - time_before_ns)
                         sample_ns.append(res)
+
                 results[func].append(np.mean(sample))
-                results_time[func].append(np.mean(sample))
+                results_time[func].append(np.mean(sample_times))
+                results_n[func].append(np.mean(n_obl))
                 results_file.write(f'{np.mean(sample)}\n')
                 results_time_file.write(f'{np.mean(sample_times)}\n')
+                result_n_file.write(f'{n_obl}\n')
 
     results_file.close()
     results_time_file.close()
+    result_n_file.close()
     
     for func in lof:
         plt.plot(list(range(minimum,len(distance_matrix)+1,step)),results[func],label=func)
@@ -536,5 +596,51 @@ def pilot_experiment(lof,distance_matrix,minimum,step):
         
     plt.xlim(minimum,len(distance_matrix))
     plt.legend()
+
+    plt.show()
+
+##define the graph
+matrix_1= create_matrix(1000)
+matrix = np.loadtxt("matrix.csv", delimiter=",")
+graph = Graph(matrix)
+n = len(matrix)
+names = np.arange(0,n)
+graph.add_edge(matrix,names)
+
+def create_plots(file_name, minimum, maximum,step):
+    colors = {'nearest_neighbour':"#72AED8",'simulated_annealing':"#FCB67A",'ant_system':"#6FCA6F",'random_swaping':"#E27F80",'two_opt':"#B48CD6"}
+    result = open(file_name,'r')
+    res = dict()
+    name = ""
+    for raw in result:
+        try:
+            raw = raw.replace("\n","")
+            row = float(raw)
+            res[name].append(row)
+        except:
+            res[raw] = []
+            name = raw
+    funcs = list(res.keys())
+    
+    plt.figure(figsize=(10, 6), dpi=100)
+
+    for func in funcs:
+        print(list(range(minimum,(maximum+1), step)))
+
+        plt.plot(list(range(minimum,(maximum+1), step)), res[func],
+                 label=func, color=colors[func], linewidth=2, alpha=0.8)
+        
+    plt.legend(frameon=True, fontsize=10, loc='upper left')
+
+    ax = plt.gca() # Get Current Axis
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    
+    plt.yscale('log', base=2)
+
+    plt.xlabel('Liczba miast', fontsize=12, labelpad=10, color='#333333')
+    plt.ylabel('Obliczona wielkość funkcji celu', fontsize=12, labelpad=10, color='#333333')
+
+    ax.grid(True, which='both',linestyle=':', alpha=0.6)
 
     plt.show()
